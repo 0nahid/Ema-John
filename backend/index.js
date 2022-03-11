1e126cf579dbf67114f29bad2e955dbde2997d5b
Nahid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-03-11 12:50:02 +0600

    require the dotenv & added db username & pass

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
+// require the dotenv variable
+require("dotenv").config();
+
 app.get("/", (req, res) =>
   res.send({ status: `Server is running on port ${port}` })
 );
